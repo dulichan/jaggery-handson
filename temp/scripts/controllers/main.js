7344d159ab801109b3d79f79175f998d1b9292db
Chan <dulithaz@gmail.com>
2014-02-18 15:33:24 +0530

    Initial commit

---
+++ temp/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('3rdMeetupApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
