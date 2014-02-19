af0c87d055d1f7f311eea605c28e13e77bbd87b7
Chan <dulithaz@gmail.com>
2014-02-19 14:31:53 +0530

    Sorted out for blogging

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,24 +1,10 @@
 'use strict';
-angular.module('3rdMeetupApp').controller('MainCtrl', function($scope, $http) {
-    var BS = function() {
-        alert("Better call Saul!!!");
-    }
-    var list_todos = function() {
-        $http.get("api/todos").success(function(data) {
-            $scope.todos = data;
-        });
-    }
-    $scope.check = function(todo){
-  		$http.put("api/todos", {id:todo.id, status:todo.status}).success(function(){
-  			list_todos();
-  		}).error(BS);
-  	}
-    $scope.addTodo = function() {
-        $http.post("api/todos", {
-            text: $scope.todoText
-        }).success(function() {
-            list_todos();
-        }).error(BS);
-    }
-    list_todos();
-});
+
+angular.module('3rdMeetupApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
